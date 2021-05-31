9098dc2757c2e56bd083e24877b54e8902ba7702
bigduk88 <hangdeuk@naver.com>
2021-06-01 08:32:16 +0900

    tpc 오버로딩

---
+++ src/TPC16.java
@@ -0,0 +1,12 @@
+import tpc.OverLoad;
+
+public class TPC16 {
+    public static void main(String[] args) {
+        //1+1=?, 23.4+56=?, 56.7+78.9=?
+        OverLoad ov = new OverLoad();
+        ov.hap(20, 50); // ov. haq_int_int(20, 50)
+        ov.hap(23.4f, 56);
+        ov.hap(56.7f, 78.9f);
+    }
+
+}
