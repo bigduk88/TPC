a417c56d2ca3301565f65357dc01bc7db4aedf58
bigduk88 <hangdeuk@naver.com>
2021-06-01 08:56:00 +0900

    동일한 구조, 이질적인 구조(배열 vs 클래스의 관계)

---
+++ src/TPC17.java
@@ -0,0 +1,38 @@
+import tpc.MovieVO;
+
+public class TPC17 {
+    public static void main(String[] args) {
+
+        int[ ] a = new int[5];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        a[3] = 40;
+        a[4] = 50;
+
+        int[] b = {10, 20, 30, 40, 50};
+
+        int[] c = new int[] {10, 20, 30, 40, 50};
+
+        for(int i=0; i<b.length; i++){
+            System.out.println(b[i]);
+        }
+
+        MovieVO mv = new MovieVO("비트", 12000, "연기자", 12, 1.3f);
+        System.out.println(mv.toString());
+
+        MovieVO[] marr = new MovieVO[3];
+        marr[0] = new MovieVO("비트1", 12000, "연기자1", 12, 1.3f);
+        marr[1] = new MovieVO("비트2", 13000, "연기자2", 11, 1.4f);
+        marr[2] = new MovieVO("비트3", 11000, "연기자3", 13, 1.5f);
+        System.out.println(marr[0].toString());
+        System.out.println(marr[1].toString());
+        System.out.println(marr[2].toString());
+
+        System.out.println();
+
+        for (int i = 0; i < marr.length; i++) {
+            System.out.println(marr[i]);
+        }
+    }
+}
